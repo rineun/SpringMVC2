ebd5bf9d89b2a93b5c0681e3f9b65a3688e22347
rineun <sy880808@gmail.com>
2022-05-23 15:58:16 +0900

    exception, api exception

---
+++ exception/src/main/java/hello/exception/servlet/DispatcherType.java
@@ -0,0 +1,9 @@
+package hello.exception.servlet;
+
+public enum DispatcherType {
+    FORWARD,  // MVC에서 배웠던 서블릿에서 다른 서블릿이나 JSP를 호출할 때
+    INCLUDE,  //서블릿에서 다른 서블릿이나 JSP의 결과를 포함할 때
+    REQUEST, //클라이언트
+    ASYNC,  //오류 요청
+    ERROR  //서블릿 비동기 호출
+}
